bc22ea27a4e21a78468dc3ef6c2183a5092762fe
Chris435 <reaullicekossou@gmail.com>
2020-10-14 17:41:35 +0100

    Update MainActivity.java

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -81,7 +81,11 @@ public class MainActivity extends AppCompatActivity {
     }
     // ***
     // TODO - Task 1 - Show Store Information Activity
-    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+        return super.onOptionsItemSelected(item);
+    }
     // ***
 
     private void addCandiesToDatabase(Candy[] candies) {
